ec6b6d619a476229b6662ef6c3d6c5935a9d5b6a
tsaiminghan <minghan.tsai@gmail.com>
2020-09-02 22:03:36 +0800

    Update www_ranwen_la.py

--- novelwebsite/www_ranwen_la.py
+++ novelwebsite/www_ranwen_la.py
@@ -1,4 +1,11 @@
-from .sample1 import mydl
+from .sample1 import mydl as base
+
+# work at 2020/09/02
+class mydl(base):
+  def chapter_link(self, link):
+    if not link.startswith('http'):
+      link = 'https://www.ranwen.la' + link
+    return link
 
 if __name__ == '__main__':
   a = mydl('https://www.ranwen.la/files/article/89/89698/')
